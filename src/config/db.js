5bac932eb9c96f5644f170555670db4d8e265d56
jephthe <jephthepetitfrere@gmail.com>
2024-11-26 22:19:53 -0500

    update

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,13 @@
 import mongoose from 'mongoose';
-import { DB_URI } from './dotenvConfig.js';
+import { DB_URI } from '../dotenvConfig.js';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(DB_URI); 
     console.log('Database connected successfully');
   } catch (error) {
     console.error('Database connection failed:', error.message);
-    process.exit(1); // Exit process if connection fails
+    process.exit(1);
   }
 };
 
